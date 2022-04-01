f3897423059bc236cfaa8ede28320f270c305690
Jari Sääksi <jari.saaksi@gmail.com>
2022-04-01 14:23:03 +0300

    First test added

---
+++ tests.py
@@ -0,0 +1,10 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+def test_homepage(client):
+    response = client.get('/')
+    assert response.status_code == 200
